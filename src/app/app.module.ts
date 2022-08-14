cffe156736602b29e9cf3404be46ebdcbb82452a
Laurin Wolf <laurin@l5w.de>
2022-08-15 00:59:22 +0200

    add basic pwa stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { ColorPickerComponent } from './color-picker/color-picker.component';
 import { PreviousDevicesComponent } from './previous-devices/previous-devices.component';
 import { AutoconnectSelectorComponent } from './autoconnect-selector/autoconnect-selector.component';
 import { FormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,12 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
